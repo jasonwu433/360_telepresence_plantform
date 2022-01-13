1e47db7915d8dde64965573496de9261a8a9a023
HITL167\Yuanjie <yuanjie.wu@canterbury.ac.nz>
2022-01-13 17:49:28 +1300

    360 Camera Streaming setup
    1. Add drop list feature to choose streaming resolution.
    2. fix 360 texture outside wrap to inside wrap.
    3. VR camera is still not working which will cause crash.

---
+++ Assets/Scripts/RicohThetaV.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RicohThetaV : MonoBehaviour
+{
+	public streamingResolutions streamingRes;
+	public enum streamingResolutions { R_FullHD, R_4K }; // Set streaming resolution profiles	
+	public const int THETA_V_AUDIO_NUMBER = 0;
+
+	private string camName;
+	private string RICOH_DRIVER_NAME = "";
+	private AudioSource audioSource;
+
+	void Start()
+	{
+		ReadSteamingCamera();
+		ProcessStreamingAudio();
+		ProcessStreamingTexture();
+		InvertSphere();
+	}
+
+	private void ReadSteamingCamera()
+	{
+		RICOH_DRIVER_NAME = (streamingRes == streamingResolutions.R_FullHD) ? "RICOH THETA V FullHD" : "RICOH THETA V 4K";
+
+		WebCamDevice[] devices = WebCamTexture.devices; // Get devices list
+		Debug.Log("Number of web cams connected: " + devices.Length);
+		for (int i = 0; i < devices.Length; i++)
+		{
+			Debug.Log(i + " " + devices[i].name);
+			if (devices[i].name == RICOH_DRIVER_NAME)
+			{
+				camName = devices[i].name;
+			}
+		}
+
+		Debug.Log($"I am using the webcam named {camName}");
+
+		if (camName != RICOH_DRIVER_NAME)
+		{
+			Debug.Log("ERROR: " + RICOH_DRIVER_NAME +
+				" not found. Install Ricoh streaming driver from https://topics.theta360.com/uk/faq/c_06_v/304_1/. Make sure your camera is in live streaming mode");
+		}
+	}
+
+	private void ProcessStreamingTexture()
+	{
+		Renderer rend = GetComponent<Renderer>();
+		WebCamTexture mycam = new WebCamTexture(camName);
+		rend.material.mainTexture = mycam;
+		mycam.Play();
+	}
+
+	private void ProcessStreamingAudio()
+	{
+		audioSource = GetComponent<AudioSource>();
+		string[] audioDevices = Microphone.devices;
+
+		for (int i = 0; i < audioDevices.Length; i++)
+		{
+			Debug.Log(i + " " + audioDevices[i]);
+		}
+
+		audioSource.clip = Microphone.Start(audioDevices[THETA_V_AUDIO_NUMBER], true, 10, 44100);
+		audioSource.loop = true;
+		while (!(Microphone.GetPosition(null) > 0)) { }
+		audioSource.Play();
+	}
+
+	private void InvertSphere()
+	{
+		Vector3[] normals = GetComponent<MeshFilter>().mesh.normals;
+
+		for (int i = 0; i < normals.Length; i++)
+		{
+			normals[i] = -normals[i]; // point the normal from outside to inside
+		}
+
+		GetComponent<MeshFilter>().sharedMesh.normals = normals;
+
+		int[] triangles = GetComponent<MeshFilter>().sharedMesh.triangles;
+
+		for (int i = 0; i < triangles.Length; i += 3)
+		{
+			int t = triangles[i];
+			triangles[i] = triangles[i + 2];
+			triangles[i + 2] = t;
+		}
+
+		GetComponent<MeshFilter>().sharedMesh.triangles = triangles;
+	}
+}
